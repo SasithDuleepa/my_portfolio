96e54bffa1e5a5cde6cdcf3df9781096868b1b98
sasith duleepa <duleepajayathissa@gmail.com>
2024-10-04 11:47:57 +0530

    responsive

--- src/contact/contact.jsx
+++ src/contact/contact.jsx
@@ -18,7 +18,7 @@ export default function Contact() {
   
     // Pass the form object directly as the third argument
     emailjs
-      .sendForm('service_zlsr3r4', 'template_l7tg2uo', form.current, '0q0ak5Ga51v88ziyp')
+      .sendForm(`${process.env.REACT_APP_EMAILJS_SERVICE_ID}`, `${process.env.REACT_APP_EMAILJS_TEMPLATE_ID}`, form.current, `${process.env.REACT_APP_EMAILJS_PUBLIC_KEY}`)
       .then(
         () => {
           console.log('SUCCESS!');
@@ -37,19 +37,19 @@ export default function Contact() {
       <div className='contact-div'>
         <div className='contact-info'>
           <div className='contact-info-sub'>
-            <a href='https://maps.app.goo.gl/ni6Yy1kuVH6oxAUN7'> <img src={Location} className='contact-icon' /></a>             
+            <a href='https://maps.app.goo.gl/ni6Yy1kuVH6oxAUN7'> <img alt='' src={Location} className='contact-icon' /></a>             
               <p className='contact-p3'>no.870, Unagalawehera, Polonnaruwa, Sri Lanka.</p>
           </div>
           <div className='contact-info-sub'>
-            <img src={Gmail} className='contact-icon'/>
+            <img alt='' src={Gmail} className='contact-icon'/>
             <p className='contact-p3'>duleepajayathissa@gmail.com</p>
           </div>
           <div className='contact-info-sub'>
-            <a  href='https://www.linkedin.com/in/sasith-duleepa/'>  <img src={Linkedin} className='contact-icon'/></a>
+            <a  href='https://www.linkedin.com/in/sasith-duleepa/'>  <img alt='' src={Linkedin} className='contact-icon'/></a>
             <p className='contact-p3'>Sasith Duleepa</p>
           </div>
           <div className='contact-info-sub'>
-            <a href='https://github.com/SasithDuleepa'><img src={Github} className='contact-icon'/></a>
+            <a href='https://github.com/SasithDuleepa'><img alt='' src={Github} className='contact-icon'/></a>
             <p className='contact-p3'>Sasith Duleepa</p>
           </div>
           
